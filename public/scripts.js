fe75a698a6a0ce9c368e54a2cc2bf39e37bf036b
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-14 12:43:35 -0600

    Add lock btn function

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,29 @@
-const generatePaletteBtn = document.querySelector('#generate-palette-btn');
-// const hexDisplay = document.querySelectorAll('.hex-display');
+const generatePaletteBtn = $('#generate-palette-btn');
+const lockBtn = $('.lock-btn');
 
-const generateSingleColor = (id) => {
+generatePaletteBtn.on('click', generatePalette);
+lockBtn.on('click', toggleLock);
+
+window.onload = generatePalette;
+
+function toggleLock() {
+  $(this).closest('article').toggleClass('locked');
+}
+
+function generateSingleColor(id) {
+  console.log(id)
+  if ($(id).closest('article').hasClass('locked')) { return }
+
   let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
-  let hexDisplay = document.querySelector(id);
-  hexDisplay.innerText = color
+  let hexDisplay = $(id);
+  hexDisplay.text(color);
   return color;
 }
 
-const generatePalette = () => {
-  document.querySelector('#card-1').style.backgroundColor = generateSingleColor('#card-1');
-  document.querySelector('#card-2').style.backgroundColor = generateSingleColor('#card-2');
-  document.querySelector('#card-3').style.backgroundColor = generateSingleColor('#card-3');
-  document.querySelector('#card-4').style.backgroundColor = generateSingleColor('#card-4');
-  document.querySelector('#card-5').style.backgroundColor = generateSingleColor('#card-5');
+function generatePalette() {
+  $('#card-1').css('background-color', generateSingleColor('.card-1'));
+  $('#card-2').css('background-color', generateSingleColor('.card-2'));
+  $('#card-3').css('background-color', generateSingleColor('.card-3'));
+  $('#card-4').css('background-color', generateSingleColor('.card-4'));
+  $('#card-5').css('background-color', generateSingleColor('.card-5'));
 }
-
-window.onload = generatePalette;
-generatePaletteBtn.addEventListener('click', generatePalette);
