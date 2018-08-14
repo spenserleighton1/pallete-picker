f4a4b58eee956f0dea964f845cf764df70f9b354
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-14 12:44:10 -0600

    :sparkles:

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const app = express();
 
 app.use(express.static('public'))
 
+
+app.locals.projects = []
+app.locals.projects = [{id: '1', name: 'ppicker'},{id: '2', name: 'wow'}]
+
+app.get('/api/v1/projects', (request, response) => {
+  const projects = app.locals.projects;
+  response.status(200).json(projects);
+});
+
+app.get('/api/v1/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const project = app.locals.projects.find(project => project.id === id);
+  return response.status(200).json(project);
+});
+
+app.post('/api/v1/projects', (request, response) => {
+  const id = Date.now();
+  const { project } = request.body;
+
+  app.locals.projects.push(project)
+
+  response.status(201).json({ id, project })
+})
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
