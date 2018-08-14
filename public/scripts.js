35136fd0951b12d945089e0c8a11ec26b4eae7bb
Spenser Leighton <spenser.leighton@gmail.com>
2018-08-13 18:53:01 -0600

    Add hex display styles to cards

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,19 @@
 const generatePaletteBtn = document.querySelector('#generate-palette-btn');
+// const hexDisplay = document.querySelectorAll('.hex-display');
 
-
-
-const generateSingleColor = () => {
+const generateSingleColor = (id) => {
   let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+  let hexDisplay = document.querySelector(id);
+  hexDisplay.innerText = color
   return color;
 }
 
 const generatePalette = () => {
-  document.querySelector('#card-1').style.backgroundColor = generateSingleColor();
-  document.querySelector('#card-2').style.backgroundColor = generateSingleColor();
-  document.querySelector('#card-3').style.backgroundColor = generateSingleColor();
-  document.querySelector('#card-4').style.backgroundColor = generateSingleColor();
-  document.querySelector('#card-5').style.backgroundColor = generateSingleColor();
+  document.querySelector('#card-1').style.backgroundColor = generateSingleColor('#card-1');
+  document.querySelector('#card-2').style.backgroundColor = generateSingleColor('#card-2');
+  document.querySelector('#card-3').style.backgroundColor = generateSingleColor('#card-3');
+  document.querySelector('#card-4').style.backgroundColor = generateSingleColor('#card-4');
+  document.querySelector('#card-5').style.backgroundColor = generateSingleColor('#card-5');
 }
 
 window.onload = generatePalette;
